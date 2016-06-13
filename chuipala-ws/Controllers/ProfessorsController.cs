501b5861beedb8434900b58df0600d22cd71f818
Jessie Jadas <jessiejadas@gmail.com>
2016-06-13 10:15:08 +0200

    structure

---
+++ chuipala-ws/Controllers/ProfessorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using chuipala_ws.Models;
+
+namespace chuipala_ws.Controllers
+{
+    public class ProfessorsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Professors
+        public ActionResult Index()
+        {
+            return View(db.Professors.ToList());
+        }
+
+        // GET: Professors/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Professor professor = db.Professors.Find(id);
+            if (professor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(professor);
+        }
+
+        // GET: Professors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Professors/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,FirstName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Professors.Add(professor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(professor);
+        }
+
+        // GET: Professors/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Professor professor = db.Professors.Find(id);
+            if (professor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(professor);
+        }
+
+        // POST: Professors/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,FirstName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(professor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(professor);
+        }
+
+        // GET: Professors/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Professor professor = db.Professors.Find(id);
+            if (professor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(professor);
+        }
+
+        // POST: Professors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Professor professor = db.Professors.Find(id);
+            db.Professors.Remove(professor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ chuipala-ws/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace chuipala_ws.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Professor> Professors { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        
+    }
+}
